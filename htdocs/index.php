bc1db43908f49c2a72bfc759ebf9552be7080af5
Sridhar M <sridhar2do@gmail.com>
2016-10-11 18:42:55 +0530

    API Execution

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,29 @@
 <?php
 
-	include './error.php';
+	include './error.inc.php';
+	include './pineAuth.inc.php';
 
-?>
+	$wsdl = "https://novaserviceuat.pinelabs.com/Nova/PineGiftWebService.svc?wsdl";
+
+	$client = new SoapClient( $wsdl );
+
+	$params = array (
+		"AccessCode" => $accessCode,
+		"ServerId" => $accessCode
+	);
+
+	// Get the response
+	$response = $client->GetBrandConfiguration( $params );
+
+	// Output from the response
+	print_r( $response );
+
+	echo "\n\n\n";
+	
+
+	// Get the available Functions and Type
+	echo '<pre>';
+	var_dump($client->__getFunctions()); 
+	echo "\n\n\n";
+	var_dump($client->__getTypes()); 
+	echo '</pre>';
